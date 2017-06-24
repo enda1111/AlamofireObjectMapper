0f4fac5aef782a4158663a614e5824756a129cba
Ryan Zulkoski <rzulkoski@hayneedle.com>
2017-06-24 11:02:04 -0500

    Excluded AlamofireObjectMapperTests from Package.swift.

--- Package.swift
+++ Package.swift
@@ -6,5 +6,5 @@ let package = Package(
 	.Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4),
         .Package(url: "https://github.com/Hearst-DD/ObjectMapper.git", majorVersion: 2, minor: 2)
     ],
-    exclude: []
+    exclude: ["AlamofireObjectMapperTests"]
 )
