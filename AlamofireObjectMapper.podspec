c8e58451beeed3288b23057650e37ddff41f9d87
Ryan Zulkoski <rzulkoski@hayneedle.com>
2019-03-27 13:01:13 -0500

    Migrated to Swift 4.

--- AlamofireObjectMapper.podspec
+++ AlamofireObjectMapper.podspec
@@ -15,6 +15,6 @@ Pod::Spec.new do |s|
 
   s.requires_arc = 'true'
   s.source_files = 'AlamofireObjectMapper/**/*.swift'
-  s.dependency 'Alamofire', '~> 4.1'
-  s.dependency 'ObjectMapper', '~> 2.0'
+  s.dependency 'Alamofire', '~> 4.8'
+  s.dependency 'ObjectMapper', '~> 3.4'
 end
--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "AlamofireObjectMapper",
     dependencies : [
 	.Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4),
-        .Package(url: "https://github.com/Hearst-DD/ObjectMapper.git", majorVersion: 2, minor: 2)
+        .Package(url: "https://github.com/Hearst-DD/ObjectMapper.git", majorVersion: 3, minor: 4)
     ],
     exclude: ["AlamofireObjectMapperTests"]
 )
